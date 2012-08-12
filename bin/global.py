59a550d4ae046839ff50c800e74532aab7dff563
Clint Sharp <clint@typhoon.org>
2012-08-11 19:19:18 -0700

    first commit

---
+++ bin/global.py
@@ -0,0 +1,49 @@
+import sys
+sys.path.append('lib')
+
+from config import Config
+import logging, logging.handlers
+import json
+import urllib2, urllib
+import time
+
+c = Config()
+logger = logging.getLogger('appnet')
+
+out = logging.getLogger('appnet_global')
+formatter = logging.Formatter('%(message)s')
+handler = logging.handlers.RotatingFileHandler(filename=c.file_name, maxBytes=c.max_bytes,
+                                                backupCount=c.backup_files)
+handler.setFormatter(formatter)
+out.addHandler(handler)
+out.setLevel(logging.DEBUG)
+logger.debug("Configured to log to '%s' with maxBytes '%s' with backupCount '%s'" % \
+                (c.file_name, c.max_bytes, c.backup_files))
+
+
+while True:
+    url = c.global_stream_url
+    query_string = { 'min_id': c.min_id }
+    url = url + '?' + urllib.urlencode(query_string)
+    headers = { 'Authorization': 'Bearer '+c.access_token }
+    req = urllib2.Request(url, None, headers)
+    response = urllib2.urlopen(req)
+    page = response.read()
+    results = json.loads(page)
+
+    i = 0
+    for item in results:
+        # logger.debug('Item ID: %s Min ID: %s' % (item['id'], c.min_id))
+        if long(item['id']) > long(c.min_id):
+            i += 1
+            out.debug(json.dumps(item))
+
+    logger.debug("Retrieved %s results from app.net.  Last ID: %s.  Wrote %s results." 
+                % (len(results), results[0]['id'], i))
+
+    # If we pass a greater min ID to App.Net than exists, it'll give us back the last 20 results
+    # better to send it the current max id and always ignore it
+    if long(results[0]['id']) > long(c.min_id):
+        c.set_min_id(long(results[0]['id']))
+
+    time.sleep(c.sleep_time)
---
+++ lib/config.py
@@ -0,0 +1,54 @@
+from ConfigParser import ConfigParser
+import logging, logging.handlers
+import os
+
+
+class Config:
+	# Stolen from http://code.activestate.com/recipes/66531/
+    # This implements a Borg patterns, similar to Singleton
+    # It allows numerous instantiations but always shared state
+    __sharedState = {}
+
+    _conf = None
+
+    max_bytes = None
+    backup_files = None
+    file_name = None
+
+    access_token = None
+    global_stream_url = None
+    min_id = None
+
+    sleep_time = None
+
+    parent_dir = None
+
+    def __init__(self):
+        # Rebind the internal datastore of the class to an Instance variable
+        self.__dict__ = self.__sharedState
+
+        logger = logging.getLogger('appnet')
+        logger.propagate = False
+        logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        streamHandler = logging.StreamHandler()
+        streamHandler.setFormatter(formatter)
+        logger.addHandler(streamHandler)
+
+        self.grandparent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+        self._conf = ConfigParser()
+        self._conf.read([self.grandparent_dir+'/conf/global.conf'])
+
+        for sect in self._conf.sections():
+            for item in self._conf.items(sect):
+                setattr(self, item[0], item[1])
+
+                if item[0] == 'sleep_time':
+                    setattr(self, item[0], float(item[1]))
+
+    def set_min_id(self, min_id):
+        self.min_id = min_id
+        self._conf.set('appnet', 'min_id', min_id)
+        f = open(self.grandparent_dir+'/conf/global.conf', 'w')
+        self._conf.write(f)
